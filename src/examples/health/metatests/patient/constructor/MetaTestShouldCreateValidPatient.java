03ee5b8fbf30b4836baf9706830c13ffe6961577
Lewis Baumstark (Univ. of West GA) <32909052+lewisb-uwg@users.noreply.github.com>
2022-10-31 14:52:56 -0400

    Convert these meta-tests to use our Expectations framework:
    noted that static assertEquals also cause problems when faked.

--- src/examples/health/metatests/patient/constructor/MetaTestShouldCreateValidPatient.java
+++ src/examples/health/metatests/patient/constructor/MetaTestShouldCreateValidPatient.java
@@ -2,6 +2,7 @@ package health.metatests.patient.constructor;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.doubleoops.heavymeta.Expectations;
 import org.doubleoops.heavymeta.HeavyMeta;
 import static org.doubleoops.heavymeta.SafeAssertions.*;
 import org.junit.jupiter.api.Assertions;
@@ -22,175 +23,187 @@ public class MetaTestShouldCreateValidPatient {
 	
 	@Test
 	public void shouldHaveArrangeActStage() {
-		var fakePatient = new MockUp<Patient>() {
-			public boolean didCreate  = false;
-			public String firstName = null;
-			public String lastName = null;
-			public int age = Integer.MIN_VALUE;
+		var expectations = new Expectations() {
+
+			boolean didCreatePatient  = false;
+			String patientFirstName = null;
+			String patientLastName = null;
+			int patientAge = Integer.MIN_VALUE;
+			
+			@Override
+			protected void establishExpectations() {
+				expect(didCreatePatient,
+						"Did not instantiate a Patient object in your Arrange stage");
+				
+				expect(patientAge > 0,
+						"Patient constructor's age parameter should be > 0, based on the @precondition");
+				
+				expect(patientFirstName != null,
+						"Patient constructor's firstName parameter should not be null, based on the @precondition");
+				
+				expect(!patientFirstName.isEmpty(),
+						"Patient constructor's firstName parameter should not be empty, based on the @precondition");
+				
+				expect(patientLastName != null,
+						"Patient constructor's lastName parameter should not be null, based on the @precondition");
+				
+				expect(!patientLastName.isEmpty(),
+						"Patient constructor's lastName parameter should not be empty, based on the @precondition");
+				
+			}			
+		};
+		
+		new MockUp<Patient>() {
+			
 			
 			@Mock
 			public void $init(Invocation inv, String firstName, String lastName, int age) {
-				didCreate = true;
+				expectations.didCreatePatient = true;
 				
 				// capture these to test for validity later
-				this.firstName = firstName;
-				this.lastName = lastName;
-				this.age = age;
+				expectations.patientFirstName = firstName;
+				expectations.patientLastName = lastName;
+				expectations.patientAge = age;
 				inv.proceed(firstName, lastName, age);
 			}
 		};
 		
 		metaTester.runStudentsTestIgnoreFails();
-		
-		assertTrue(fakePatient.didCreate,
-				"Did not instantiate a Patient object in your Arrange stage");
-		
-		assertTrue(fakePatient.age > 0,
-				"Patient constructor's age parameter should be > 0, based on the @precondition");
-		
-		assertNotNull(fakePatient.firstName,
-				"Patient constructor's firstName parameter should not be null, based on the @precondition");
-		
-		assertNotEquals(fakePatient.firstName, "",
-				"Patient constructor's firstName parameter should not be empty, based on the @precondition");
-		
-		assertNotNull(fakePatient.lastName,
-				"Patient constructor's lastName parameter should not be null, based on the @precondition");
-		
-		assertNotEquals(fakePatient.lastName, "",
-				"Patient constructor's lastName parameter should not be empty, based on the @precondition");
+		expectations.assertPassed();
 	}
 	
 	@Test
 	public void shouldHaveAssertForFirstName() {
 		final String bogusString = "ImaBogusString!@#$";
 		
-		var fakePatient = new MockUp<Patient>() {
+		var expectations = new Expectations() {
+			String patientFirstName = "";
+			boolean hasFirstNameAssertion = false;
+			String expectedFirstName = "";
 			
-			public String firstName = "";
+			@Override
+			protected void establishExpectations() {
+				expect(hasFirstNameAssertion,
+						"It seems you do not have an assertEquals whose expected value is the same as your constructor's firstName, and whose actual value is returned by getFirstName()");
+				
+				expect(patientFirstName.equals(expectedFirstName),
+						"The expected value of your assertEquals for the first name does not match the firstName you gave to the constructor");
+			}
+		};
+		
+		new MockUp<Patient>() {
 			@Mock
 			public String getFirstName(Invocation inv) {
 				// capture for use later
-				firstName = inv.proceed();
+				expectations.patientFirstName = inv.proceed();
 				
 				// return bogus first name so we can identify this result later
 				return bogusString;
 			}
 		};
 		
-		var fakedAssertions = new MockUp<Assertions>() {
-			
-			public static boolean hasFirstNameAssertion = false;
-			public static String expectedFirstName = "";
-			public static boolean assertEqualsUsesGetFirstName = false;
-			
+		new MockUp<Assertions>() {
 			@Mock
-			public static void assertEquals(Object expected, Object actual) {
+			public void assertEquals(Object expected, Object actual) {
 				if (actual.equals(bogusString)) {
-					hasFirstNameAssertion = true;
-					expectedFirstName = (String)expected;
-					assertEqualsUsesGetFirstName = true;
+					expectations.hasFirstNameAssertion = true;
+					expectations.expectedFirstName = (String)expected;
 				}
 			}
 			
 		};
 		
 		metaTester.runStudentsTestIgnoreFails();
-		
-		safeAssertTrue(fakedAssertions.hasFirstNameAssertion,
-				"It seems you do not have an assertEquals whose expected value is the same as your constructor's firstName, and whose actual value is returned by getFirstName()");
-		
-		safeAssertEquals(fakePatient.firstName, fakedAssertions.expectedFirstName,
-				"The expected value of your assertEquals for the first name does not match the firstName you gave to the constructor");
-		
+		expectations.assertPassed();	
 	}
 
 	@Test
 	public void shouldHaveAssertForLastName() {
 		final String bogusString = "ImaBogusString!@#$";
 		
-		var fakePatient = new MockUp<Patient>() {
+		var expectations = new Expectations() {
+			String patientLastName = "";
+			boolean hasLastNameAssertion = false;
+			String expectedLastName = "";
 			
-			public String lastName = "";
+			@Override
+			protected void establishExpectations() {
+				expect(hasLastNameAssertion,
+						"It seems you do not have an assertEquals whose expected value is the same as your constructor's lastName, and whose actual value is returned by getLastName()");
+				
+				expect(patientLastName.equals(expectedLastName),
+						"The expected value of your assertEquals for the last name does not match the lastName you gave to the constructor");
+			}
+		};
+		
+		new MockUp<Patient>() {
 			@Mock
 			public String getLastName(Invocation inv) {
 				// capture for use later
-				lastName = inv.proceed();
+				expectations.patientLastName = inv.proceed();
 				
 				// return bogus last name so we can identify this result later
 				return bogusString;
 			}
 		};
 		
-		var fakedAssertions = new MockUp<Assertions>() {
-			
-			public static boolean hasLastNameAssertion = false;
-			public static String expectedLastName = "";
-			public static boolean assertEqualsUsesGetLastName = false;
-			
+		new MockUp<Assertions>() {			
 			@Mock
-			public static void assertEquals(Object expected, Object actual) {
+			public void assertEquals(Object expected, Object actual) {
 				if (actual.equals(bogusString)) {
-					hasLastNameAssertion = true;
-					expectedLastName = (String)expected;
-					assertEqualsUsesGetLastName = true;
+					expectations.hasLastNameAssertion = true;
+					expectations.expectedLastName = (String)expected;
 				}
 			}
-			
 		};
 		
 		metaTester.runStudentsTestIgnoreFails();
-		
-		safeAssertTrue(fakedAssertions.hasLastNameAssertion,
-				"It seems you do not have an assertEquals whose expected value is the same as your constructor's lastName, and whose actual value is returned by getLastName()");
-		
-		safeAssertEquals(fakePatient.lastName, fakedAssertions.expectedLastName,
-				"The expected value of your assertEquals for the last name does not match the lastName you gave to the constructor");
-		
+		expectations.assertPassed();
 	}
 
 	@Test
 	public void shouldHaveAssertForAge() {
 		final int bogusAge = -429818;
 		
-		var fakePatient = new MockUp<Patient>() {
+		var expectations = new Expectations() {
+			int patientAge = Integer.MIN_VALUE;
+			boolean hasAgeAssertion = false;
+			int expectedAge = Integer.MIN_VALUE;
 			
-			public int age = Integer.MIN_VALUE;
+			@Override
+			protected void establishExpectations() {
+				expect(hasAgeAssertion,
+						"It seems you do not have an assertEquals whose expected value is the same as your constructor's age, and whose actual value is returned by getAge()");
+				
+				expect(patientAge == expectedAge,
+						"The expected value of your assertEquals for the age does not match the age you gave to the constructor");
+			}
+		};
+		
+		new MockUp<Patient>() {
 			@Mock
 			public int getAge(Invocation inv) {
 				// capture for use later
-				age = inv.proceed();
+				expectations.patientAge = inv.proceed();
 				
 				// return bogus age so we can identify this result later
 				return bogusAge;
 			}
 		};
 		
-		var fakedAssertions = new MockUp<Assertions>() {
-			
-			public static boolean hasAgeAssertion = false;
-			public static int expectedAge = Integer.MIN_VALUE;
-			public static boolean assertEqualsUsesGetAge = false;
-			
+		new MockUp<Assertions>() {
 			@Mock
-			public static void assertEquals(int expected, int actual) {
+			public void assertEquals(int expected, int actual) {
 				if (actual == bogusAge) {
-					hasAgeAssertion = true;
-					expectedAge = expected;
-					assertEqualsUsesGetAge = true;
+					expectations.hasAgeAssertion = true;
+					expectations.expectedAge = expected;
 				}
 			}
 			
 		};
 		
 		metaTester.runStudentsTestIgnoreFails();
-		
-		safeAssertTrue(fakedAssertions.hasAgeAssertion,
-				"It seems you do not have an assertEquals whose expected value is the same as your constructor's age, and whose actual value is returned by getAge()");
-		
-		safeAssertEquals(fakePatient.age, fakedAssertions.expectedAge,
-				"The expected value of your assertEquals for the age does not match the age you gave to the constructor");
-		
+		expectations.assertPassed();
 	}
 
 }
