e48d2dc33d6cad4e56495a7c625de4898ddc8b7c
Lewis Baumstark <lewis.baumstark.jr@gmail.com>
2024-07-30 10:42:29 -0400

    Clean the xml reports directory before each run:
    rationale: irritatingly, JUnit5's implementation of the open xml report
    standard will ONLY write to a file, not create an in-memory object. So
    this is part of a hack where we pick that file out of the reports
    directory and use it later on in our run. The easiest way to make sure
    we're getting the latest version of the data is by wiping the previous
    version.
    
    This may bite us in the...um...KNEE...later.

--- src/main/java/org/doubleoops/mosh/MoshEngine.java
+++ src/main/java/org/doubleoops/mosh/MoshEngine.java
@@ -2,7 +2,9 @@ package org.doubleoops.mosh;
 
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Path;
 
@@ -17,11 +19,13 @@ import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 import org.junit.platform.reporting.legacy.xml.LegacyXmlReportGeneratingListener;
 import org.junit.platform.reporting.open.xml.OpenTestReportGeneratingListener;
-
+import org.apache.commons.io.FileUtils;
 /**
  * Executor for the mosh tool.
  */
 public class MoshEngine {
+
+	private static final String OPEN_XML_REPORT_DIR = "./open-xml-reports";
 
 	//private static final UniqueId ID = UniqueId.root("mosh","engine");
 	/**
@@ -40,12 +44,23 @@ public class MoshEngine {
 						metaTestFilter
 					)
 					.configurationParameter("junit.platform.reporting.open.xml.enabled", "true")
-					.configurationParameter("junit.platform.reporting.output.dir", "./open-xml-reports")
+					.configurationParameter("junit.platform.reporting.output.dir", OPEN_XML_REPORT_DIR)
 					.build();
+		
+		cleanReportDirectory();
 		
 		try (LauncherSession session = LauncherFactory.openSession()) {
 			TestPlan testPlan = session.getLauncher().discover(discoveryRequest);
 			session.getLauncher().execute(testPlan);
+		}
+	}
+
+	private static void cleanReportDirectory() {
+		try {
+			FileUtils.cleanDirectory(new File(OPEN_XML_REPORT_DIR));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
